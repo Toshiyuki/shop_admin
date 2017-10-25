c9cca4a6c3527b8e3d9514e6d447e5150eab8550
Toshiyuki Miyazawa <toshi@nomade.biz>
2017-10-25 13:21:21 +0900

    change migrations

--- database/migrations/2017_10_24_042145_create_items_table.php
+++ database/migrations/2017_10_24_042145_create_items_table.php
@@ -15,6 +15,20 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('code',60);//コード番号
+            $table->string('name');//商品名
+            $table->string('abname',40);//省略名
+            $table->string('artist');//アーティスト名
+            $table->string('img');//商品画像
+            $table->integer('price');//値段
+            $table->string('expl');//商品説明
+            $table->text('detail');//詳細
+            $table->text('records');//収録楽曲
+            $table->integer('amounts');//在庫数
+            $table->integer('postage');//送料
+            $table->integer('orderlimit');//一回のオーダー制限
+            $table->string('category',100);
+            $table->integer('shopid');//ショップID
             $table->timestamps();
         });
     }
